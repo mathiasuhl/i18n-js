34886c129ce63c9e2ba7bec499c2db92166d9c8a
PikachuEXE <pikachuexe@gmail.com>
2014-01-03 17:35:25 +0800

    * Undo commit https://github.com/fnando/i18n-js/commit/4e5c525ff6e1ec4d3449852746fa5651a0577d68

--- lib/i18n/js/engine.rb
+++ lib/i18n/js/engine.rb
@@ -8,11 +8,11 @@ module I18n
           next unless JS::Dependencies.using_asset_pipeline?
           next unless Rails.configuration.assets.compile
 
-          registry = Sprockets.respond_to?("register_preprocessor") ? Sprockets : Rails.application.assets
+          Rails.application.assets.register_preprocessor "application/javascript", :"i18n-js_dependencies" do |context, source|
+            if context.logical_path == "i18n/filtered"
+              ::I18n.load_path.each {|path| context.depend_on(File.expand_path(path))}
+            end
 
-          registry.register_preprocessor "application/javascript", :"i18n-js_dependencies" do |context, source|
-            next source unless context.logical_path == "i18n/filtered"
-            ::I18n.load_path.each {|path| context.depend_on(File.expand_path(path))}
             source
           end
         end
